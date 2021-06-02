a6d8cb24c2a97372662601f328e9f0c1cd26580e
Swaraj Gandhi <swaraj@browserstack.com>
2021-06-02 13:57:24 +0530

    added new function to common

--- packages/common/lib/common.js
+++ packages/common/lib/common.js
@@ -1,13 +1,9 @@
-"use strict";
-
-// module.exports = common;
-
-// function common() {
-//     // TODO
-// }
-
 import lodash from "lodash";
 
 export const cloneDeepFromLodash = (object) => {
   return lodash.cloneDeep(object);
+};
+
+export const concat = (array, value) => {
+  return lodash.concat(array, value);
 };
